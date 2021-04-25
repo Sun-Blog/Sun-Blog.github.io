c1c089908140214f17991672335bad2eea9930c1
王铁新 <1575374226@qq.com>
2021-04-25 14:41:29 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:sun-blog/sun-blog.github.io.git master
+git push -f https://github.com/sun-blog/sun-blog.github.io.git master
 
 cd ../
 
