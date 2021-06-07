149c9dcf6d16ed0c5b487d44e7a37008f4be2e39
王铁新 <1575374226@qq.com>
2021-06-07 09:25:23 +0800

    push

--- push.sh
+++ push.sh
@@ -1,3 +1,6 @@
 git add .
 git commit -m 'push'
 git push -f https://github.com/sun-blog/sun-blog.github.io.git docs
+
+# git config --global --unset http.proxy 
+# git config --global --unset https.proxy
