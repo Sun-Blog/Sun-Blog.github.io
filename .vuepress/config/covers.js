54c259aef43441739a9682f82ab8881688f758b0
王铁新 <1575374226@qq.com>
2021-06-09 20:53:35 +0800

    push

--- .vuepress/config/covers.js
+++ .vuepress/config/covers.js
@@ -1,9 +1,9 @@
 module.exports = [
     'https://p6-tt.byteimg.com/origin/pgc-image/3af1476b709148c39f8b2e2dc51c7338.jpg',
-    'https://p26-tt.byteimg.com/origin/pgc-image/30b3c84206ca4547baf6c57ac7bc1c0b.jpg',
-    'http://browser9.qhimg.com/bdr/__85/t01a676f085326404d3.jpg',
-    'http://browser9.qhimg.com/bdr/__85/t01735cb17ef572c6ec.jpg',
+    'http://p4.qhimg.com/bdr/__85/t017577e4c5cb6a113c.jpg',
+    'http://p0.qhimg.com/bdr/__85/t0122b0e1af805d3679.jpg',
+    'http://p8.qhimg.com/bdr/__85/t01f31b4860117e2f76.jpg',
     'http://p4.qhimg.com/bdr/__85/t012cde4a5058c156b7.jpg',
-    'http://p6.qhimg.com/bdr/__85/t017979183edf68561b.jpg',
-    'http://p4.qhimg.com/bdr/__85/t019a8699854767bd4a.jpg',
+    'http://p18.qhimg.com/bdr/__85/t01ac43fe30b4bb7cfc.jpg',
+    'http://p15.qhimg.com/bdr/__85/t01d6f5fd3610da0a08.jpg',
 ]
